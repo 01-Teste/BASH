0fdd745e286dc185d2cc63b74fcd1400f6a281ea
drop-dev <59323037+drop-dev@users.noreply.github.com>
2021-03-26 20:15:33 -0300

    Update main.sh

--- LIST-INTERFACE/main.sh
+++ LIST-INTERFACE/main.sh
@@ -14,10 +14,6 @@ while true; do clear; MENU && if [ -z $OP1 ]; then echo ""; echo $MSG1; sleep 1
 
             if [ -z $OP2 ]; then echo ""; echo $MSG2; sleep 1
             elif [ $OP2 = 1 ]; then echo -e $MSG3; read ETH && if [ -z $ETH ]; then echo ""; echo $MSG4; sleep 1; clear; else clear; ifconfig $ETH; SAIR; fi
-            elif [ $OP2 = 0 ]; then sleep 1; clear; break; else echo ""; echo $MSG4; sleep 1; fi
-            
-        done
+            elif [ $OP2 = 0 ]; then sleep 1; clear; break; else echo ""; echo $MSG4; sleep 1; fi; done
 
-    elif [ $OP1 = 0 ]; then sleep 1; clear; break; else echo ""; echo $MSG4; sleep 1; fi
-    
-done
+    elif [ $OP1 = 0 ]; then sleep 1; clear; break; else echo ""; echo $MSG4; sleep 1; fi; done
